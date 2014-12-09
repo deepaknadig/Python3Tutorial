38339616fc12538c6b11414f9c77dc8ed46b6218
Deepak Nadig Anantha <deepak@solutt.com>
2014-12-09 08:45:24 +0530

    Updated Sphinx Configuration for easydev

--- docs/conf.py
+++ docs/conf.py
@@ -14,11 +14,14 @@
 
 import sys
 import os
+import easydev
+from easydev import copybutton
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath("."))
 
 # -- General configuration ------------------------------------------------
 
@@ -28,7 +31,15 @@ import os
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = []
+extensions = ['sphinx.ext.autodoc','sphinxcontrib.autorun']
+extensions.append('easydev.copybutton')
+jscopybutton_path = easydev.copybutton.get_copybutton_path()
+
+if os.path.isdir('_static')==False:
+    os.mkdir('_static')
+
+import shutil
+shutil.copy(jscopybutton_path, '_static')
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
